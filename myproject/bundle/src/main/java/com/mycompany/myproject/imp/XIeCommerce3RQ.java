44ce6921a593a67bba6b603e4ccf30f077d33c84
Chaudhary <monali.chaudhary@capgemini.com>
2017-07-18 12:34:20 +0530

    Signed-off-by: Chaudhary <monali.chaudhary@capgemini.com>

--- myproject/bundle/src/main/java/com/mycompany/myproject/imp/XIeCommerce3RQ.java
+++ myproject/bundle/src/main/java/com/mycompany/myproject/imp/XIeCommerce3RQ.java
@@ -69,7 +69,7 @@ public class XIeCommerce3RQ extends SlingAllMethodsServlet{
 		logger.info("***********************************************"+postPacket);
 		
 		out.println(postPacket.toString());
-		out.println("Hello World : Jenkins auto deploymen 333");
+		out.println("Hello World : Jenkins auto deploymen 444");
 		
 	    AccessTokenResponsePacket accessTokenResponsePacket = AccessTokenUtility.GetPostAccessToken(strGUID, strPSK, strURL, postPacket);
 	   if (!(accessTokenResponsePacket.getRequestError() + "").equals("null")) {
